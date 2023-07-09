66c621bb235cd4c825fdbf6000412e1270f04479
Rahul Behera <rahulbehera2002@gmail.com>
2023-07-09 17:14:36 +0530

    feat: add highlight effect to about section

--- src/modules/About/AboutDetails/index.tsx
+++ src/modules/About/AboutDetails/index.tsx
@@ -18,47 +18,47 @@ const AboutDetails = ({ username, dob, email, workExperiences, educations, skill
     <div className='about-details'>
       <div className='highlight'><span className='purple'>class</span> <span className='yellow'>{username}</span>{' {'}</div>
       <div className='highlight'><span className='dot'>··</span><span className='comment'>// I did, because I can.</span></div>
-      <div><span className='dot'>··</span><span className='comment'>// My vast variety of skills is continuously expanding.</span></div>
+      <div className='highlight'><span className='dot'>··</span><span className='comment'>// My vast variety of skills is continuously expanding.</span></div>
       <div>
-        <div><span className='dot'>..</span><span className='purple'>constructor</span>() {'{'}</div>
-        <div><span className='dot'>....</span><span className='this'>this</span>.<span className='green'>name</span> <span className='this'>=</span> <span className='neon'>{`'${username}'`}</span>;</div>
-        <div><span className='dot'>....</span><span className='this'>this</span>.<span className='green'>dayOfBirthTimestamp</span> <span className='this'>=</span> <span className='orange'>{`${dob}`}</span>; <span className='dot'></span><span className='comment'>// UNIX Timestamp.</span></div>
-        <div><span className='dot'>....</span><span className='this'>this</span>.<span className='green'>email</span> <span className='this'>=</span> <span className='neon'>{`'${email}'`}</span>;</div>
-        <div><span className='dot'>··</span>{'}'}</div>
+        <div className='highlight'><span className='dot'>..</span><span className='purple'>constructor</span>() {'{'}</div>
+        <div className='highlight'><span className='dot'>....</span><span className='this'>this</span>.<span className='green'>name</span> <span className='this'>=</span> <span className='neon'>{`'${username}'`}</span>;</div>
+        <div className='highlight'><span className='dot'>....</span><span className='this'>this</span>.<span className='green'>dayOfBirthTimestamp</span> <span className='this'>=</span> <span className='orange'>{`${dob}`}</span>; <span className='dot'></span><span className='comment'>// UNIX Timestamp.</span></div>
+        <div className='highlight'><span className='dot'>....</span><span className='this'>this</span>.<span className='green'>email</span> <span className='this'>=</span> <span className='neon'>{`'${email}'`}</span>;</div>
+        <div className='highlight'><span className='dot'>··</span>{'}'}</div>
       </div>
       <div>
-        <div><span className='dot'>..</span><span className='yellow'>workExperience</span>() {'{'}</div>
-        <div><span className='dot'>....</span><span className='purple'>return</span> {'['}</div>
+        <div className='highlight'><span className='dot'>..</span><span className='yellow'>workExperience</span>() {'{'}</div>
+        <div className='highlight'><span className='dot'>....</span><span className='purple'>return</span> {'['}</div>
         {
           workExperiences.map( wE => (
-            <div><span className='dot'>......</span>{'{'} <span className='neon'>{`'${wE.timeline}'`}</span> : <span className='neon'>{`'${wE.location}'`}</span> {'},'}</div>
+            <div className='highlight'><span className='dot'>......</span>{'{'} <span className='neon'>{`'${wE.timeline}'`}</span> : <span className='neon'>{`'${wE.location}'`}</span> {'},'}</div>
           ))
         }
-        <div><span className='dot'>....</span>{'];'}</div>
-        <div><span className='dot'>..</span>{'}'}</div>
+        <div className='highlight'><span className='dot'>....</span>{'];'}</div>
+        <div className='highlight'><span className='dot'>..</span>{'}'}</div>
       </div>
       <div>
-        <div><span className='dot'>..</span><span className='yellow'>education</span>() {'{'}</div>
-        <div><span className='dot'>....</span><span className='purple'>return</span> {'['}</div>
+        <div className='highlight'><span className='dot'>..</span><span className='yellow'>education</span>() {'{'}</div>
+        <div className='highlight'><span className='dot'>....</span><span className='purple'>return</span> {'['}</div>
         {
           educations.map( education => (
-            <div><span className='dot'>......</span>{'{'} <span className='neon'>{`'${education.timeline}'`}</span> : <span className='neon'>{`'${education.location}'`}</span> {'},'}</div>
+            <div className='highlight'><span className='dot'>......</span>{'{'} <span className='neon'>{`'${education.timeline}'`}</span> : <span className='neon'>{`'${education.location}'`}</span> {'},'}</div>
           ))
         }
-        <div><span  className='dot'>....</span>{'];'}</div>
-        <div><span className='dot'>..</span>{'}'}</div>
+        <div className='highlight'><span  className='dot'>....</span>{'];'}</div>
+        <div className='highlight'><span className='dot'>..</span>{'}'}</div>
       </div>
       <div>
-        <div><span className='dot'>..</span><span className='yellow'>skills</span>() {'{'}</div>
-        <div><span className='dot'>....</span><span className='purple'>return</span> {'['}</div>
+        <div className='highlight'><span className='dot'>..</span><span className='yellow'>skills</span>() {'{'}</div>
+        <div className='highlight'><span className='dot'>....</span><span className='purple'>return</span> {'['}</div>
         {
           skills.map( skill => (
-            <div><span className='dot'>......</span><span className='neon'>{`'${skill}'`}</span>{','} </div>
+            <div className='highlight'><span className='dot'>......</span><span className='neon'>{`'${skill}'`}</span>{','} </div>
           ))
         }
-        <div><span className='dot'>....</span>{'];'}</div>
-        <div><span className='dot'>..</span>{'}'}</div>
-        <div>{'}'}</div>
+        <div className='highlight'><span className='dot'>....</span>{'];'}</div>
+        <div className='highlight'><span className='dot'>..</span>{'}'}</div>
+        <div className='highlight'>{'}'}</div>
       </div>
     </div>
   );
